4a85412b54844fd708081ddffa826f33b75b3934
ShMuhammad <14.chaouni@gmail.com>
2024-04-15 20:58:08 +0100

    prettierrc created

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
-require('dotenv').config();
+require("dotenv").config();
 
-const Discord = require('discord.js');
-const openai = require('openai');
+const Discord = require("discord.js");
+const openai = require("openai");
 
 const client = new Discord.Client();
 // Use process.env.DISCORD_TOKEN to access your Discord token
@@ -9,7 +9,7 @@ client.login(process.env.DISCORD_TOKEN);
 
 // Use process.env.OPENAI_API_KEY to access your OpenAI API key
 openai.configure({
-  apiKey: process.env.OPENAI_API_KEY
+  apiKey: process.env.OPENAI_API_KEY,
 });
 
 // Now you can safely use your API keys without hard-coding them into your project
